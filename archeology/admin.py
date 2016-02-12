5022d314e9fe4ebbfc0e23ad1b2d2075e0049f09
Eddy Ernesto del Valle Pino <xigmatron@gmail.com>
2016-02-13 00:49:17 +0100

    Create admin interface.

--- archeology/admin.py
+++ archeology/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Material, Decoration, Color, Finding, Artifact
+
+
+class ArtifactAdmin(admin.ModelAdmin):
+
+    date_hierarchy = 'found_date'
+    exclude = ('found_date',)
+    filter_horizontal = ('colors', 'decorations',)
+    list_display = ('name', 'main_material', 'get_colors', 'found')
+    search_fields = (
+        'name',
+        'description',
+        'main_material__name',
+        'decorations__name',
+        'colors__name',
+        'found__place',
+        'found__discoverers__name',
+        'found__discoverers__last_name',
+    )
+
+
+class FindingAdmin(admin.ModelAdmin):
+
+    filter_horizontal = ('discoverers',)
+
+
+admin.site.register(Material)
+admin.site.register(Decoration)
+admin.site.register(Color)
+admin.site.register(Finding, FindingAdmin)
+admin.site.register(Artifact, ArtifactAdmin)
